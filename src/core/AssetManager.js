5a81d5619e96ee4a73c5c645e00e4acc707e713e
alvindlc0824-ux <alvindlc0824@gmail.com>
2026-09-10 22:43:40 +0800

    Delete src directory

--- src/core/AssetManager.js
+++
@@ -1,5 +0,0 @@
-import {GLTFLoader} from 'three/addons/loaders/GLTFLoader.js';
-export class AssetManager{
-constructor(){this.loader=new GLTFLoader();this.cache=new Map()}
-async loadModel(url){if(this.cache.has(url))return this.cache.get(url);const g=await this.loader.loadAsync(url);this.cache.set(url,g);return g}
-}
